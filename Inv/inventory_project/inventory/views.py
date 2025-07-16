38e721d0d6754c6160e868631495e32bae10140d
links-astec <gabbyquaye2021@gmail.com>
2025-07-16 11:11:57 -0100

    adding the frontend

--- Inv/inventory_project/inventory/views.py
+++ Inv/inventory_project/inventory/views.py
@@ -8,6 +8,57 @@ from django.contrib.auth.models import User
 from .models import Product, Buyer, Transaction, Notification
 from .serializers import ProductSerializer, BuyerSerializer, TransactionSerializer, NotificationSerializer
 from .services import record_transaction
+# views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.utils.timezone import now, timedelta
+from .models import Transaction
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.db.models import Sum, F
+from .models import Product, Transaction
+from .serializers import ProductSerializer
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def sales_report(request):
+    total_transactions = Transaction.objects.filter(user=request.user).count()
+    total_revenue = Transaction.objects.filter(user=request.user).aggregate(
+        total=Sum(F('quantity_sold') * F('price_per_item'))
+    )['total'] or 0
+
+    low_stock_products = Product.objects.filter(user=request.user, quantity__lte=5)
+    low_stock_serialized = ProductSerializer(low_stock_products, many=True).data
+
+    report = {
+        'total_transactions': total_transactions,
+        'total_revenue': total_revenue,
+        'low_stock_products': low_stock_serialized,
+    }
+
+    return Response(report)
+
+class SalesSummaryView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        today = now().date()
+        week_ago = today - timedelta(days=7)
+
+        transactions = Transaction.objects.filter(user=request.user)
+        today_sales = transactions.filter(date_sold__date=today)
+        week_sales = transactions.filter(date_sold__date__gte=week_ago)
+
+        data = {
+            "total_transactions": transactions.count(),
+            "total_revenue": sum(t.total_price for t in transactions),
+            "today_sales_count": today_sales.count(),
+            "today_revenue": sum(t.total_price for t in today_sales),
+            "week_revenue": sum(t.total_price for t in week_sales),
+        }
+        return Response(data)
 
 
 @api_view(['POST'])
@@ -39,6 +90,8 @@ class NotificationViewSet(viewsets.ModelViewSet):
 
     def perform_update(self, serializer):
         serializer.save(user=self.request.user)
+
+
 
 
 class TransactionViewSet(viewsets.ViewSet):
--- Inv/inventory_project/inventory_project/settings.py
+++ Inv/inventory_project/inventory_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'inventory',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 
 ]
 
@@ -54,6 +55,7 @@ REST_FRAMEWORK = {
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -64,6 +66,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'inventory_project.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://your-frontend-domain.com",
+]
+
 
 TEMPLATES = [
     {
